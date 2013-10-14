3464d927af832fd6d2685477e8235cf3c5f9685a
Jorge Ruesga <jorge@ruesga.com>
2013-10-14 02:24:55 +0200

    Frameworks: QS performance profiles tile
    Patchset 2: Fix not null comparation
    Patchset 3: xxhdpi drawables
    Patchset 4: Fix xxhdpi drawables (thx blunden)
                Rebased
    Patchset 5: Add suggestions
                Sync on boot the system property with the setting
    Patchset 6: Rebased
    
    Change-Id: Ib32d3734b63f88a3473e0ab10d8edf30dae9ceb6
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Conflicts:
    
    	core/res/res/values-el/mahdi_strings.xml
    	core/res/res/values-it/mahdi_strings.xml
    	core/res/res/values-ru/mahdi_strings.xml
    	core/res/res/values/config.xml
    	packages/SystemUI/res/values/mahdi_arrays.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -40,6 +40,7 @@ import static com.android.internal.util.mahdi.QSConstants.TILE_NETWORKMODE;
 import static com.android.internal.util.mahdi.QSConstants.TILE_NETWORKTRAFFIC;
 import static com.android.internal.util.mahdi.QSConstants.TILE_NFC;
 import static com.android.internal.util.mahdi.QSConstants.TILE_ONTHEGO;
+import static com.android.internal.util.mahdi.QSConstants.TILE_PERFORMANCE_PROFILE;
 import static com.android.internal.util.mahdi.QSConstants.TILE_PROFILE;
 import static com.android.internal.util.mahdi.QSConstants.TILE_QUIETHOURS;
 import static com.android.internal.util.mahdi.QSConstants.TILE_RINGER;
@@ -96,6 +97,7 @@ import com.android.systemui.quicksettings.NetworkTrafficTile;
 import com.android.systemui.quicksettings.NetworkAdbTile;
 import com.android.systemui.quicksettings.NfcTile;
 import com.android.systemui.quicksettings.OnTheGoTile;
+import com.android.systemui.quicksettings.PerformanceProfileTile;
 import com.android.systemui.quicksettings.PreferencesTile;
 import com.android.systemui.quicksettings.ProfileTile;
 import com.android.systemui.quicksettings.QuickSettingsTile;
@@ -197,7 +199,7 @@ public class QuickSettingsController {
 
         if (!lteSupported) {
             TILES_DEFAULT.remove(TILE_LTE);
-        }        
+        }
 
         if (!gpsSupported) {
             TILES_DEFAULT.remove(TILE_LOCATION);
@@ -272,6 +274,10 @@ public class QuickSettingsController {
                 mTileStatusUris.add(Settings.System.getUriFor(Settings.System.SYSTEM_PROFILES_ENABLED));
                 if (QSUtils.systemProfilesEnabled(resolver)) {
                     qs = new ProfileTile(mContext, this);
+                }
+            } else if (tile.equals(TILE_PERFORMANCE_PROFILE)) {
+                if (QSUtils.deviceSupportsPerformanceProfiles(mContext)) {
+                    qs = new PerformanceProfileTile(mContext, this);
                 }
             } else if (tile.equals(TILE_NFC)) {
                 // User cannot add the NFC tile if the device does not support it
