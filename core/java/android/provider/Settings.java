89ab76961a9edbe68e734fecd5c5704c63b6861a
Alexander Martinz <eviscerationls@gmail.com>
2014-01-22 01:02:55 +0100

    [1/2] SystemUI: Add brightness slider to notification drawer
    adds a brightness slider below the expanded statusbar header and
    (if enabled) quick settings ribbons
    
    also wrap an if around gesture recorder in the fling methods, only
    call them if DEBUG is enabled.
    
    Change-Id: I65077f5ddb20aa9f13813eedfaa0cb09a667eb88
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4357,6 +4357,12 @@ public final class Settings {
         public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
 
         /**
+         * Show brightness slider in notification panel
+         * @hide
+         */
+        public static final String NOTIFICATION_BRIGHTNESS_SLIDER = "notification_brightness_slider";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
