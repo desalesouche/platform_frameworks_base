e127cdc3b5d2e332fe1079282041947021020370
Jorge Ruesga <jorge@ruesga.com>
2014-02-09 23:12:33 +0100

    core: add support for translucent volume panel
    Add support for display the volume panel in translucent mode
    
    Patchset 4: Make it depends to high-end graphics flag
    
    Change-Id: I4f21a6cc88282789072a0e71ae696b754a5e083e
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Conflicts:
    
    	core/java/android/view/VolumePanel.java

--- core/java/android/view/VolumePanel.java
+++ core/java/android/view/VolumePanel.java
@@ -18,6 +18,11 @@ package android.view;
 
 import com.android.internal.R;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface.OnDismissListener;
@@ -43,6 +48,7 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.WindowManager.LayoutParams;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
@@ -112,6 +118,9 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     public static final int VOLUME_OVERLAY_EXPANDED = 2;
     public static final int VOLUME_OVERLAY_NONE = 3;
 
+    private static final int TRANSLUCENT_START_LEVEL = 160;
+    private static final int TRANSLUCENT_TO_OPAQUE_DURATION = 400;
+
     protected Context mContext;
     private AudioManager mAudioManager;
     protected AudioService mAudioService;
@@ -120,6 +129,10 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     private boolean mVoiceCapable;
     private boolean mVolumeLinkNotification;
     private int mCurrentOverlayStyle = -1;
+
+    private final boolean mTranslucentDialog;
+    private boolean mShouldRunDropTranslucentAnimation = false;
+    private boolean mRunningDropTranslucentAnimation = false;
 
     // True if we want to play tones on the system stream when the master stream is specified.
     private final boolean mPlayMasterStreamTones;
@@ -283,6 +296,7 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
         mContext = context;
         mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         mAudioService = volumeService;
+        mTranslucentDialog = ActivityManager.isHighEndGfx();
 
         // For now, only show master volume if master volume is supported
         boolean useMasterVolume = context.getResources().getBoolean(
@@ -370,6 +384,8 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
 
         mMoreButton.setOnClickListener(this);
         listenToRingerMode();
+
+        applyTranslucentWindow();
     }
 
     private void changeOverlayStyle(int newStyle) {
@@ -1078,6 +1094,7 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
 
             case MSG_TIMEOUT: {
                 if (mDialog.isShowing()) {
+                    applyTranslucentWindow();
                     mDialog.dismiss();
                     mActiveStreamType = -1;
                 }
@@ -1137,6 +1154,9 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     }
 
     public void onStartTrackingTouch(SeekBar seekBar) {
+        if (mTranslucentDialog && mShouldRunDropTranslucentAnimation) {
+            startRemoveTranslucentAnimation();
+        }
     }
 
     public void onStopTrackingTouch(SeekBar seekBar) {
@@ -1156,8 +1176,7 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     public void onClick(View v) {
         if (mTranslucentDialog && mShouldRunDropTranslucentAnimation) {
             startRemoveTranslucentAnimation();
-        }
-        if (v == mMoreButton) {
+        } else if (v == mMoreButton) {
             expand();
         } else if (v instanceof ImageView) {
             Intent volumeSettings = new Intent(android.provider.Settings.ACTION_SOUND_SETTINGS);
@@ -1175,8 +1194,8 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
         if (!mTranslucentDialog || mRunningDropTranslucentAnimation) return;
 
         mPanel.getBackground().setAlpha(TRANSLUCENT_START_LEVEL);
-        mMoreButton.setAlpha(TRANSLUCENT_START_LEVEL);
-        mDivider.setAlpha(TRANSLUCENT_START_LEVEL);
+        mMoreButton.setAlpha(0.0f);
+        mDivider.setAlpha(0.0f);
         mShouldRunDropTranslucentAnimation = true;
     }
 
