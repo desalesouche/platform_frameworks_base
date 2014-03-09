22d5518a635b52bc7a8c1b80cf684aea0f23a747
Jmz <jmztaylor@gmail.com>
2014-03-09 08:28:00 +0100

    FWB: Allow disabling of FC dialogs (1/2)
    Change-Id: If87aa74ebb45849f6e5fe3a53ce9beeaac28f624
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4351,6 +4351,12 @@ public final class Settings {
         public static final String RECENT_PANEL_FAVORITES = "recent_panel_favorites";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
