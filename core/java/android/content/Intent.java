76bdbf78d15a840a663a43350c6642749a79188e
Chintan Shah <cgintan1998@gmail.com>
2014-03-31 11:05:35 -0600

    derp..didn't get saved
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- core/java/android/content/Intent.java
+++ core/java/android/content/Intent.java
@@ -3704,14 +3704,14 @@ public class Intent implements Parcelable, Cloneable {
      * If set, this intent will always match start up as a floating window
      * in multi window scenarios.
      */
-    public static final int FLAG_FLOATING_WINDOW = 0x00001000;
+    public static final int FLAG_FLOATING_WINDOW = 0x00002000;
     /**
      * If set in an Intent passed to {@link Context#startActivity Context.startActivity()},
      * this flag will cause a newly launching task to be resized according to the split
      * view metrics, making it running alongside another app.
      * @hide
      */
-    public static final int FLAG_ACTIVITY_SPLIT_VIEW = 0x00002000;
+    public static final int FLAG_ACTIVITY_SPLIT_VIEW = 0x00001000;
 
     /**
      * If set, when sending a broadcast only registered receivers will be
