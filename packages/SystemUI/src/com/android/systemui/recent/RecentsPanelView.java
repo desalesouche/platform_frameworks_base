6c103bcc5288a4ae665a6864b55aa2b4cf30df6f
Chintan Shah <cgintan1998@gmail.com>
2014-04-17 20:16:06 -0600

    Revert "Recents: force clear caches on longclick"
    This reverts commit 59c5f6f3bfe32d30453e55959f21a15d11f5453e.

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -77,12 +77,6 @@ import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.List;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.lang.Runtime;
 
 public class RecentsPanelView extends FrameLayout implements OnItemClickListener, RecentsCallback,
         StatusBarPanel, Animator.AnimatorListener {
@@ -515,23 +509,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 @Override
                 public void onClick(View v) {
                     ((ViewGroup) mRecentsContainer).clearAllNonLocked();
-                }
-            });
-            mClearRecents.setOnLongClickListener(new OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    clearAllNonLocked();
-                    try {
-                        ProcessBuilder pb = new ProcessBuilder("su", "-c", "/system/bin/sh");
-                        OutputStreamWriter osw = new OutputStreamWriter(pb.start().getOutputStream());
-                        osw.write("sync" + "\n" + "echo 3 > /proc/sys/vm/drop_caches" + "\n");
-                        osw.write("\nexit\n");
-                        osw.flush();
-                        osw.close();
-                    } catch (Exception e) {
-                        Log.d(TAG, "Flush caches failed!");
-                    }
-                    return true;
                 }
             });
         }
