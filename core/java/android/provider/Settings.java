6c36f6f8932e04f21398e0e8f51a064d9078b7a7
Mahdi-Rom <mahdi.rom@gmail.com>
2014-05-31 17:03:11 +0100

    Revert "Add an option to allow a (subtle) notification vibration during calls."
    This reverts commit fee6ed0e9a52765713bd47b857dab408dbe3d740.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4508,14 +4508,6 @@ public final class Settings {
          public static final String SWAP_VOLUME_KEYS_ON_ROTATION = "swap_volume_keys_on_rotation";
 
         /**
-         * Whether to allow notification vibration while notification alerts are disabled
-         * (e.g. during phone calls). The vibration pattern to be used will be a subtle one;
-         * custom vibration is disabled at that point.
-         * @hide
-         */
-        public static final String NOTIFICATION_VIBRATE_DURING_ALERTS_DISABLED = "vibrate_while_no_alerts";
-
-        /**
          * Whether to not showing active display when there is annoying notifications.
          * Set the timeout of peek when pikcing up the device
          * @hide
@@ -4651,7 +4643,6 @@ public final class Settings {
             LOCKSCREEN_SOUNDS_ENABLED,
             SHOW_WEB_SUGGESTIONS,
             NOTIFICATION_LIGHT_PULSE,
-            NOTIFICATION_VIBRATE_DURING_ALERTS_DISABLED,
             SIP_CALL_OPTIONS,
             SIP_RECEIVE_CALLS,
             POINTER_SPEED,
