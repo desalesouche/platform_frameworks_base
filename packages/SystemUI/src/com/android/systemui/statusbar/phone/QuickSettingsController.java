CyanogenMod/android_frameworks_base

QuickSettings : Add BatterySaver Tile (1/2)

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -58,6 +58,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_PROFILE;
 import static com.android.internal.util.slim.QSConstants.TILE_CAMERA;
 import static com.android.internal.util.slim.QSConstants.TILE_INTERNALMEMORY;
 import static com.android.internal.util.slim.QSConstants.TILE_ONTHEGO;
+import static com.android.internal.util.slim.QSConstants.TILE_BATTERYSAVER;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -115,6 +116,7 @@ import com.android.systemui.quicksettings.FChargeTile;
 import com.android.systemui.quicksettings.ProfileTile;
 import com.android.systemui.quicksettings.CameraTile;
 import com.android.systemui.quicksettings.OnTheGoTile;
+import com.android.systemui.quicksettings.BatterySaverTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -272,6 +274,8 @@ public class QuickSettingsController {
                 qs = new InternalMemory(mContext, this);
             } else if (tile.equals(TILE_ONTHEGO)) {
                 qs = new OnTheGoTile(mContext, this);
+            } else if (tile.equals(TILE_BATTERYSAVER)) {
+                qs = new BatterySaverTile(mContext, this);
             } else if (tile.equals(TILE_PROFILE)) {
                 mTileStatusUris.add(Settings.System.getUriFor(Settings.System.SYSTEM_PROFILES_ENABLED));
                 if (DeviceUtils.systemProfilesEnabled(resolver)) {

desalesouche/platform_frameworks_base

Conflicts:
	core/java/com/android/internal/util/mahdi/QSConstants.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -20,6 +20,7 @@ import static com.android.internal.util.mahdi.QSConstants.TILES_DEFAULT;
 import static com.android.internal.util.mahdi.QSConstants.TILE_AIRPLANE;
 import static com.android.internal.util.mahdi.QSConstants.TILE_AUTOROTATE;
 import static com.android.internal.util.mahdi.QSConstants.TILE_BATTERY;
+import static com.android.internal.util.mahdi.QSConstants.TILE_BATTERYSAVER;
 import static com.android.internal.util.mahdi.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.mahdi.QSConstants.TILE_BRIGHTNESS;
 import static com.android.internal.util.mahdi.QSConstants.TILE_CAMERA;
@@ -74,6 +75,7 @@ import com.android.systemui.quicksettings.AirplaneModeTile;
 import com.android.systemui.quicksettings.AlarmTile;
 import com.android.systemui.quicksettings.AutoRotateTile;
 import com.android.systemui.quicksettings.BatteryTile;
+import com.android.systemui.quicksettings.BatterySaverTile;
 import com.android.systemui.quicksettings.BluetoothTile;
 import com.android.systemui.quicksettings.BrightnessTile;
 import com.android.systemui.quicksettings.BugReportTile;
@@ -87,6 +89,8 @@ import com.android.systemui.quicksettings.LteTile;
 import com.android.systemui.quicksettings.MobileNetworkTile;
 import com.android.systemui.quicksettings.MobileNetworkTypeTile;
 import com.android.systemui.quicksettings.MusicTile;
+import com.android.systemui.quicksettings.NavBarTile;
+import com.android.systemui.quicksettings.NetworkTrafficTile;
 import com.android.systemui.quicksettings.NetworkAdbTile;
 import com.android.systemui.quicksettings.NfcTile;
 import com.android.systemui.quicksettings.OnTheGoTile;
@@ -106,8 +110,6 @@ import com.android.systemui.quicksettings.VolumeTile;
 import com.android.systemui.quicksettings.RemoteDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
-import com.android.systemui.quicksettings.NavBarTile;
-import com.android.systemui.quicksettings.NetworkTrafficTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -293,6 +295,8 @@ public class QuickSettingsController {
                 }
             } else if (tile.equals(TILE_ONTHEGO)) {
                 qs = new OnTheGoTile(mContext, this);
+            } else if (tile.equals(TILE_BATTERYSAVER)) {
+                qs = new BatterySaverTile(mContext, this);
             }
 
             if (qs != null) {