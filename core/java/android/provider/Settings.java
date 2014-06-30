burstlam/android_frameworks_base

AppCircleSideBar : Custom TriggerWidth (1/2)

core/java/android/provider/Settings.java
@@ -5383,6 +5383,12 @@ public final class Settings {
         public static final String WHITELIST_APP_CIRCLE_BAR = "whitelist_app_circle_bar";
 
         /**
+         * Set app circle bar trigger width.
+         * @hide
+         */
+        public static final String APP_CIRCLE_SIDEBAR_TRIGGER_WIDTH = "app_circle_bar_trigger_width";
+
+        /**
          * Whether to enable voice wakeup.  The value is boolean (1 or 0).
          * @hide
          */

packages/SystemUI/src/com/android/systemui/appcirclesidebar/AppCircleSidebar.java
@@ -27,6 +27,7 @@ import android.view.WindowManager;
 import android.widget.*;
 
 import com.android.systemui.R;
+import com.android.systemui.chaos.TriggerOverlayView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -124,6 +125,8 @@ public class AppCircleSidebar extends FrameLayout implements PackageAdapter.OnCi
                     Settings.System.ENABLE_APP_CIRCLE_BAR), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.WHITELIST_APP_CIRCLE_BAR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.APP_CIRCLE_SIDEBAR_TRIGGER_WIDTH), false, this);
             update();
         }
 
@@ -144,6 +147,10 @@ public class AppCircleSidebar extends FrameLayout implements PackageAdapter.OnCi
             String includedApps = Settings.System.getStringForUser(resolver,
                     Settings.System.WHITELIST_APP_CIRCLE_BAR,
                     UserHandle.USER_CURRENT_OR_SELF);
+            int width = Settings.System.getInt(
+                    resolver, Settings.System.APP_CIRCLE_SIDEBAR_TRIGGER_WIDTH, 15);
+            if (mTriggerWidth != width)
+                setTriggerWidth(width);
             if (mPackageAdapter != null) {
                 mPackageAdapter.createIncludedAppsSet(includedApps);
                 mPackageAdapter.reloadApplications();
@@ -480,5 +487,15 @@ public class AppCircleSidebar extends FrameLayout implements PackageAdapter.OnCi
         TaskStackBuilder.create(mContext)
                 .addNextIntentWithParentStack(intent).startActivities();
         showAppContainer(false);
+    }
+
+    protected void setTriggerWidth(int value) {
+        mLayoutParams = (WindowManager.LayoutParams)this.getLayoutParams();
+        mTriggerWidth = value;
+        mLayoutParams.width = mTriggerWidth;
+        try {
+            mWM.updateViewLayout(this, mLayoutParams);
+        } catch (Exception e) {
+        }
     }
 }

desalesouche/platform_frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/src/com/android/systemui/appcirclesidebar/AppCircleSidebar.java

core/java/android/provider/Settings.java
@@ -2676,6 +2676,12 @@ public final class Settings {
         public static final String WHITELIST_APP_CIRCLE_BAR = "whitelist_app_circle_bar";
 
         /**
+         * Set app circle bar trigger width.
+         * @hide
+         */
+        public static final String APP_CIRCLE_BAR_TRIGGER_WIDTH = "app_circle_bar_trigger_width";
+
+        /**
          * Whether to show the camera widget on lockscreen
          * @hide
          */

packages/SystemUI/src/com/android/systemui/appcirclesidebar/AppCircleSidebar.java
@@ -28,6 +28,7 @@ import android.view.animation.TranslateAnimation;
 import android.widget.*;
 
 import com.android.systemui.R;
+import com.android.systemui.appcirclesidebar.TriggerOverlay;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -138,6 +139,8 @@ public class AppCircleSidebar extends FrameLayout implements PackageAdapter.OnCi
                     Settings.System.ENABLE_APP_CIRCLE_BAR), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.WHITELIST_APP_CIRCLE_BAR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.APP_CIRCLE_BAR_TRIGGER_WIDTH), false, this);
             update();
         }
 
@@ -158,6 +161,10 @@ public class AppCircleSidebar extends FrameLayout implements PackageAdapter.OnCi
             String includedApps = Settings.System.getStringForUser(resolver,
                     Settings.System.WHITELIST_APP_CIRCLE_BAR,
                     UserHandle.USER_CURRENT_OR_SELF);
+            int width = Settings.System.getInt(
+                    resolver, Settings.System.APP_CIRCLE_BAR_TRIGGER_WIDTH, 15);
+            if (mTriggerWidth != width)
+                setTriggerWidth(width);
             if (mPackageAdapter != null) {
                 mPackageAdapter.createIncludedAppsSet(includedApps);
                 mPackageAdapter.reloadApplications();
@@ -593,5 +600,15 @@ public class AppCircleSidebar extends FrameLayout implements PackageAdapter.OnCi
     public class FloatingTaskInfo {
         public IBinder packageToken;
         public String packageName;
+    }
+
+    protected void setTriggerWidth(int value) {
+        mLayoutParams = (WindowManager.LayoutParams)this.getLayoutParams();
+        mTriggerWidth = value;
+        mLayoutParams.width = mTriggerWidth;
+        try {
+            mWM.updateViewLayout(this, mLayoutParams);
+        } catch (Exception e) {
+        }
     }
 }