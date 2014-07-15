da5d2781ec08bfc077f2aa188a1eb7d01a64f39f
Chintan Shah <cgintan1998@gmail.com>
2014-07-14 23:24:26 -0600

    [1/2] Remove QS Styling options
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
@@ -44,7 +44,6 @@ public class QuickSettingsTile implements OnClickListener {
     protected int mDrawable;
     protected String mLabel;
     protected int mTileTextSize;
-    protected int mTileTextColor;
     protected int mTileTextPadding;
     protected Drawable mRealDrawable;
 
@@ -103,7 +102,6 @@ public class QuickSettingsTile implements OnClickListener {
             QuickSettingsContainerView container) {
         container.updateResources();
         mTileTextSize = container.getTileTextSize();
-        mTileTextColor = container.getTileTextColor();
         mTileTextPadding = container.getTileTextPadding();
 
         mTile = (QuickSettingsTileView) inflater.inflate(
@@ -155,9 +153,6 @@ public class QuickSettingsTile implements OnClickListener {
             tv.setText(mLabel);
             tv.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTileTextSize);
             tv.setPadding(0, mTileTextPadding, 0, 0);
-            if (mTileTextColor != -2) {
-                tv.setTextColor(mTileTextColor);
-            }
         }
         ImageView image = (ImageView) mTile.findViewById(R.id.image);
         if (image != null) {
