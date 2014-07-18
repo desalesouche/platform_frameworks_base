7b690d1978557345cf17567fdce2eb1660b5347e
chaos <slax775@gmail.com>
2014-07-18 14:48:15 -0400

    Revert "Possible fix: Battery tile coloring issues"
    This reverts commit 5c9cf76443bd6085d07dfdfc3e0be1e53350a4f3.

--- packages/SystemUI/src/com/android/systemui/quicksettings/BatteryTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/BatteryTile.java
@@ -47,9 +47,6 @@ public class BatteryTile extends QuickSettingsTile implements BatteryStateChange
     private int mBatteryLevel = 0;
     private boolean mPluggedIn;
 
-    private BatteryMeterView mBattery;
-    private BatteryCircleMeterView mCircleBattery;
-
     public BatteryTile(Context context, QuickSettingsController qsc, BatteryController controller) {
         super(context, qsc, R.layout.quick_settings_tile_battery);
 
@@ -86,15 +83,17 @@ public class BatteryTile extends QuickSettingsTile implements BatteryStateChange
 
     @Override
     public void onChangeUri(ContentResolver resolver, Uri uri) {
-        mBattery = (BatteryMeterView) mTile.findViewById(R.id.image);
-        mBattery.setColors(true);
-        mCircleBattery = (BatteryCircleMeterView) mTile.findViewById(R.id.circle_battery);
-        mCircleBattery.setColors(true);
-        if (mCircleBattery != null) {
-            mCircleBattery.updateSettings();
+        BatteryMeterView battery =
+                (BatteryMeterView) mTile.findViewById(R.id.image);
+        battery.setColors(true);
+        BatteryCircleMeterView circleBattery =
+                (BatteryCircleMeterView) mTile.findViewById(R.id.circle_battery);
+        circleBattery.setColors(true);
+        if (circleBattery != null) {
+            circleBattery.updateSettings();
         }
-        if (mBattery != null) {
-            mBattery.updateSettings();
+        if (battery != null) {
+            battery.updateSettings();
         }
         updateResources();
     }
@@ -134,8 +133,6 @@ public class BatteryTile extends QuickSettingsTile implements BatteryStateChange
                     : mContext.getString(R.string.quick_settings_battery_discharging);
             }
         }
-        mBattery.setColors(true);
-        mCircleBattery.setColors(true);
     }
 
     @Override
@@ -146,5 +143,7 @@ public class BatteryTile extends QuickSettingsTile implements BatteryStateChange
             tv.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTileTextSize);
             tv.setPadding(0, mTileTextPadding, 0, 0);
         }
+        //circleBattery.setColors(true);
+        //battery.setColors(true);
     }
 }
