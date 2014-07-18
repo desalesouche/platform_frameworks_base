a5b67173682634e822b852cde1cd6d05d1e7c7e1
Chintan Shah <cgintan1998@gmail.com>
2014-07-18 01:12:53 -0600

    Fix more things
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1073,7 +1073,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
                  if (mFloat && !"android".equals(mPkg)) {
                     Intent transparent = new Intent(mContext, com.android.systemui.Transparent.class);
-                    transparent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_FLOATING_WINDOW);
+                    transparent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
                     mContext.startActivity(transparent);
                 }
 
